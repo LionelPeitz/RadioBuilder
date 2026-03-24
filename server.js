b729579176018243a732e59ff3674f91484987a6
LionelPeitz <mrottach1129@googlemail.com>
2026-03-24 16:27:56 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ import express from "express";
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 app.get("/data", async (req, res) => {
   try {
     const response = await fetch("https://www.deutschlandfunk.de/nachrichten-100.html");
